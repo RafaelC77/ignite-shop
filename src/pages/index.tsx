cc90d74642c2e058055311a855cc87a0eabb7e0d
Rafael Guimaraes <rcarvalhobsb@gmail.com>
2022-09-21 19:53:34 -0300

    feat: add carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
 import Image from "next/future/image";
+
+import { useKeenSlider } from "keen-slider/react";
+
 import { HomeContainer, Product } from "../styles/pages/home";
+
+import "keen-slider/keen-slider.min.css";
 
 import tee1 from "../assets/tees/1.png";
 import tee2 from "../assets/tees/2.png";
 import tee3 from "../assets/tees/3.png";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={tee1} width={520} height={480} alt="" />
 
         <footer>
@@ -17,8 +29,26 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={tee2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={tee3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={tee3} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta X</strong>
