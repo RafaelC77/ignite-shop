b2d7a5c29bae42b0de6e4b38050807ee1ab917f2
Rafael Guimaraes <rcarvalhobsb@gmail.com>
2022-09-22 16:53:18 -0300

    feat: add product page layout

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,30 @@
 import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter();
-  const { id } = query;
 
-  return <p>{id}</p>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, omnis
+          illum. Cupiditate consequatur expedita ut autem impedit, odit tempore
+          quisquam nostrum neque fugiat minus quae, a adipisci veniam!
+          Consectetur, laudantium?
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
